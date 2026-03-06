0a712f494dcb3c0b10bfc8edfac00ea7a1c2e283
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2026-03-07 00:42:07 +0900

    add shadcn ui copmonents

--- lib/schema.ts
+++ lib/schema.ts
@@ -2,13 +2,42 @@ import SchemaBuilder from "@pothos/core";
 
 const builder = new SchemaBuilder({});
 
+const HELLOS = [
+  "Hello",          // English
+  "Hola",           // Spanish
+  "Bonjour",        // French
+  "Hallo",          // German
+  "Ciao",           // Italian
+  "Olá",            // Portuguese
+  "Privet",         // Russian
+  "Nǐ hǎo",         // Chinese
+  "Kon'nichiwa",    // Japanese
+  "Annyeonghaseyo", // Korean
+  "Marhaban",       // Arabic
+  "Namaste",        // Hindi
+  "Merhaba",        // Turkish
+  "Hallå",          // Swedish
+  "Geia sou",       // Greek
+  "Shalom",         // Hebrew
+  "Sawatdee",       // Thai
+  "Xin chào",       // Vietnamese
+  "Jambo",          // Swahili
+  "Cześć",          // Polish
+  "Hei",            // Finnish
+  "Sawubona",       // Zulu
+  "Szia",           // Hungarian
+];
+
 builder.queryType({
   fields: (t) => ({
     hello: t.string({
       args: {
         name: t.arg.string(),
       },
-      resolve: (parent, { name }) => `hello, ${name || "World"}`,
+      resolve: (parent, { name }) => {
+        const greeting = HELLOS[Math.floor(Math.random() * HELLOS.length)];
+        return `${greeting}, ${name || "World"}`;
+      },
     }),
   }),
 });
